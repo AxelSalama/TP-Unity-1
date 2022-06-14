38be49cd8863f5d69bae2d86cc5be9b0d08262e1
AxelSalama <axelsalama2006@gmail.com>
2022-06-14 15:50:59 -0300

    C
    C

---
+++ TP-Unity-Cuatrimeste-1-main/Assets/Scripts/Instantiate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate : MonoBehaviour
+{
+    public GameObject cubePrefab, clone;
+
+    void Update()
+    {
+         if (Input.GetKey(KeyCode.Space))
+        {
+            clone = Instantiate(cubePrefab, transform.position, Quaternion.identity);
+            Destroy(clone, 3);
+        }    
+    }
+}
